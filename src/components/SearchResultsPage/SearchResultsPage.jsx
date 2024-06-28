a22692b4dd65c9ca5101bb979d80c61ffc560041
Aima Alakhume <aimailene@gmail.com>
2024-06-28 12:23:58 -0400

    add route for search results page and include results image

--- src/components/SearchResultsPage/SearchResultsPage.jsx
+++ src/components/SearchResultsPage/SearchResultsPage.jsx
@@ -5,6 +5,10 @@ import * as React from 'react';
 const SearchResultsPage = () => {
     return (
         <>
+            <div className="container">
+                {/* <img className="bg image" src={"src/assets/images/AI_Overview_Modal_Active.png"} alt="a page of Google Search results" /> */}
+                <img className="bg image" src={"src/assets/images/AI_Overview.svg"} alt="a page of Google Search results" />
+            </div>
         </>
     )
 };
