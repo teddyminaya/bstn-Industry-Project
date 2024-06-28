1d9d39cc26828e915f469549e0fc10f64b6e3cc9
Aima Alakhume <aimailene@gmail.com>
2024-06-28 14:57:38 -0400

    finalise learn more page design

--- src/components/LearnMorePage/LearnMorePage.jsx
+++ src/components/LearnMorePage/LearnMorePage.jsx
@@ -4,8 +4,12 @@ import * as React from 'react';
 const LearnMorePage = () => {
     return (
         <>
+            <div className="learn-container">
+                <p className="hold">Google</p>
+            </div>
         </>
     )
 };
 
 export default LearnMorePage;
+
